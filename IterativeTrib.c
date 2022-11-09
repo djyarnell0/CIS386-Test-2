cf7b2344b517a386e4e04a3822126d6b8e24101f
djyarnell0 <70770731+djyarnell0@users.noreply.github.com>
2022-11-09 14:08:40 -0600

    Finished Iterative Trib

---
+++ IterativeTrib.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n, t1=2,t2=3,t3=4,tn,i;
+	n = 6;
+	printf("Enter tribonacci(0): ");
+    scanf("%d", &t1);
+    printf("Enter tribonacci(1): ");
+    scanf("%d", &t2);
+    printf("Enter tribonacci(2): ");
+    scanf("%d", &t3);
+    printf("Enter n: ");
+    scanf("%d", &n);
+    if( n==0 ){
+            tn = t1;
+            printf("tribbonacci(%d) = %d", n, tn);
+        }
+    else{
+        for(i=3;i<n;i++){
+            tn = t1 + t2 + t3;
+            t1=t2;
+            t2=t3;
+            t3=tn;
+        }
+        printf("tribbonacci(%d) = %d", n, tn);
+    }
+}
